aa02bbc3320f9396f7e33da76dc7de37a7a0b20c
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-09-22 02:16:12 -0300

    Fix: Design for happy cake

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
 import { Link } from "react-router-dom"
 
 const navigation = [
-  { name: "Home", linkTo: "/", current: true },
+  { name: "Home", linkTo: "/", current: false },
   { name: "Contact", linkTo: "/Contact", current: false },
 ]
 
@@ -13,14 +13,14 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-red-600">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-orange-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -33,12 +33,12 @@ export default function Example() {
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="block h-8 w-auto lg:hidden"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    src="https://cdn-icons-png.flaticon.com/512/2016/2016099.png"
                     alt="Your Company"
                   />
                   <img
                     className="hidden h-8 w-auto lg:block"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    src="https://cdn-icons-png.flaticon.com/512/2016/2016099.png"
                     alt="Your Company"
                   />
                 </div>
@@ -47,13 +47,7 @@ export default function Example() {
                     {navigation.map((item) => (
                       <a
                         key={item.name}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "px-3 py-2 rounded-md text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
+                        className="text-gray-300 hover:bg-orange-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                       >
                         <Link to={item.linkTo}>{item.name}</Link>
                       </a>
@@ -62,10 +56,19 @@ export default function Example() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <h1>Happy Cake</h1>
-                  </div>
+                <Menu as="div" className="relative ml-3 flex">
+                  <h1 className="text-gray-300 pr-6">Happy Cake</h1>
+
+                  <img
+                    className="block h-8 w-auto lg:hidden"
+                    src="https://cdn-icons-png.flaticon.com/512/992/992717.png"
+                    alt="Your Company"
+                  />
+                  <img
+                    className="hidden h-8 w-auto lg:block"
+                    src="https://cdn-icons-png.flaticon.com/512/992/992717.png"
+                    alt="Your Company"
+                  />
                 </Menu>
               </div>
             </div>
--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -2,11 +2,11 @@ import React from "react"
 
 function Footer() {
   return (
-    <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <footer class="p-4   shadow md:flex md:items-center md:justify-between md:p-6 bg-red-600">
       <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © 2022{" "}
-        <a href="https://flowbite.com/" class="hover:underline">
-          Flowbite™
+        <a href="" class="hover:underline">
+          Mnslvs™
         </a>
         . All Rights Reserved.
       </span>
