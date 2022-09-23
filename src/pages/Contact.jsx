b349103ccd50c2120625927b727aa28943be9aa4
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-09-23 16:16:10 -0300

    Fix: Contact and Home

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Contact() {
   return (
-    <section className="min-h-[70vh]">
+    <section className="min-h-[69vh]">
     <div className="px-4 my-2 mx-auto max-w-screen-md">
         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-gray-600">Contact Us</h2>
         <p className="mb-8 lg:mb-16 font-light text-center text-gray-900 dark:text-gray-800 sm:text-xl"></p>
